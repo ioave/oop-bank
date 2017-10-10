b6c13a2d38bafe08b3ebb39a0fd3e3e8203dc92e
Ryan Kopinsky <ryan@ioavenue.com>
2017-10-10 23:16:06 +0000

    First commit for OOP bank.

---
+++ spec/bank_account_spec.rb
@@ -0,0 +1,11 @@
+require './bank_account'
+
+describe BankAccount do
+  it "is created with an balance and the name of the client"
+  it "can report it's balance"
+  it "can make deposits"
+  it "can make withdrawals"
+  it "can transfer funds to another bank account"
+  it "has a minimum opening balance"
+  it "allows the user to change the minimum balance"
+end
