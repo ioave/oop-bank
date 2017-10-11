efcb067e97bbb8f34117b4182b3a763e1779fb68
Ryan Kopinsky <ryan@ioavenue.com>
2017-10-11 22:54:57 +0000

    Test for reporting balance is now passing.

--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -1,8 +1,16 @@
 require './bank_account'
 
 describe BankAccount do
-  it "is created with an balance and the name of the client"
-  it "can report it's balance"
+  it "is created with an balance and the name of the client" do
+    account = BankAccount.new(500, "Sarah")
+    expect(account).to be_a(BankAccount)
+  end
+
+  it "can report it's balance" do
+    account = BankAccount.new(500, "Sarah")
+    expect(account.balance).to eq(500)
+  end
+
   it "can make deposits"
   it "can make withdrawals"
   it "can transfer funds to another bank account"
