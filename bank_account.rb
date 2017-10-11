2882a64c8938ddc435aadc97bc70b5e9e1a740d6
Ryan Kopinsky <ryan@ioavenue.com>
2017-10-11 23:52:38 +0000

    Now throws an error when minimum opening balance requirment isn't met

--- bank_account.rb
+++ bank_account.rb
@@ -1,7 +1,12 @@
 class BankAccount
   attr_reader :balance
+  @@minimum_opening_balance = 200
 
   def initialize(opening_balance, account_holder)
+    if opening_balance < @@minimum_opening_balance
+      raise ArgumentError
+    end
+      
     @balance = opening_balance  
   end
     
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -33,6 +33,9 @@ describe BankAccount do
     expect(account_two.balance).to eq(700)
   end
     
-  it "has a minimum opening balance"
+  it "throws an error if minimum opening balance of 200 is not met" do
+    expect { BankAccount.new(100, "Sarah") }.to raise_error(ArgumentError)
+  end
+    
   it "allows the user to change the minimum balance"
 end
